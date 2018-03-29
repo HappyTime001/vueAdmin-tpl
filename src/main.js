86b6d6b2a72e8201cd3895dd480a12680ee822de
lihui02 <lihui02@foresee.com.cn>
2018-03-29 16:31:27 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import * as filters from './filters' // 全局vue filter
+import '@/icons' // icon
+import '@/permission' // permission control
+
+import global from '@/global/global'
+
+Vue.use(ElementUI, { locale })
+
+Vue.config.productionTip = false
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+//加载用户主题
+if( localStorage.getItem('themeValue') ){
+    global.changeTheme( localStorage.getItem("themeValue"));
+}
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
